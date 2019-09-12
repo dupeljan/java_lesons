b4e414d9700c814b7b13fb97ad5f03f2e1df3a52
Dupeljan Linux <dupeljan@gmail.com>
2019-09-12 09:31:15 +0300

    add class book

---
+++ src/main/java/com/dupel/labs/task1/WordCounter.java
@@ -0,0 +1,16 @@
+package com.dupel.labs.task1;
+
+public class WordCounter {
+    String text;
+    public WordCounter(String text){
+        this.text = text;
+    }
+
+    public int count(String word){
+        return 1;
+    }
+
+    public to_string(){
+        // word - count 
+    }
+}
